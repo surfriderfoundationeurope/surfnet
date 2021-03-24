063639cc316f2efb808006bc73029488c0f1c1e2
Mathis Chagneux <mathis.chagneux@telecom-paris.fr>
2021-03-24 08:34:10 +0100

    changing environments

--- README.md
+++ README.md
@@ -4,16 +4,9 @@
 ## Installation 
 ### Conda environments
 
-#### For use with Deeplab models 
 ```shell
 conda env create --name surfnet --file environment.yml
 conda activate surfnet 
-```
-
-#### For use with CenterNet models 
-```shell
-conda env create --name surfnet_DLA --file environment_centernet_DCN.yml
-conda activate surfnet_DLA 
 ```
 
 ### Variables
--- extract_and_save_heatmaps.py
+++ extract_and_save_heatmaps.py
@@ -4,7 +4,8 @@ from extension.datasets import SingleVideoDataset
 from base.utils.presets import HeatmapExtractPreset
 import pickle 
 import os 
-from base.centernet.models import create_model, load_model
+from base.centernet.models import create_model as create_model_centernet
+from base.centernet.models import load_model as load_model_centernet
 from common.utils import blob_for_bbox, pre_process_centernet
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -26,8 +27,8 @@ def extract(args):
         model = load_deeplab_to_cuda(args.model_name)
     else:
         transform = pre_process_centernet
-        model = create_model(arch=args.model, heads={'hm':3,'wh':2,'reg':2}, head_conv=256)
-        model = load_model(model, trained_model_weights_filename)
+        model = create_model_centernet(arch=args.model, heads={'hm':3,'wh':2,'reg':2}, head_conv=256)
+        model = load_model_centernet(model, trained_model_weights_filename)
     for param in model.parameters():
         param.requires_grad = False
     model.to('cuda')
