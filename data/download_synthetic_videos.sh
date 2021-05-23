98519ba63983f0a7a3b542e934f306857a9ac4f0
mathis <mathis.chagneux@telecom-paris.fr>
2021-05-23 14:16:30 +0200

    new download links

--- data/download_synthetic_videos.sh
+++
@@ -1,3 +0,0 @@
-wget https://partage.imt.fr/index.php/s/adwa7SpLWYadbey/download
-unzip download
-rm download
---
+++ data/download_validation_videos.sh
@@ -0,0 +1,3 @@
+wget https://partage.imt.fr/index.php/s/CWf7kPCr3Yfsrwd/download
+unzip download 
+rm download
