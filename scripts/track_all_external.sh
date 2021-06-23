8b831a9a6981bae3fc6165e750dc5e0256703280
Mathis Chagneux <mathis.chagneux@telecom-paris.fr>
2021-06-23 12:10:09 +0200

    new code to clean CVAT annotations

--- scripts/track_all_external.sh
+++ scripts/track_all_external.sh
@@ -1,11 +1,11 @@
 . scripts/shell_variables.sh 
 
-experiment_name='test'
+experiment_name='fairmot_conf_038_epoch_290'
 output_dir='experiments/tracking/'${experiment_name}
 create_clean_directory $output_dir
 
 python src/track.py \
-    --data_dir '/home/infres/chagneux/repos/FairMOT/surfrider_T1_new_data_conf_05' \
+    --data_dir '/home/infres/chagneux/repos/FairMOT/surfrider_T1_epoch_290' \
     --all_external \
     --output_dir ${output_dir} \
     --confidence_threshold 0.2 \
--- src/build_mot_for_real_videos.py
+++ src/build_mot_for_real_videos.py
@@ -1,135 +1,107 @@
-import numpy as np 
+import numpy as np
+from numpy.lib import vander 
 from common.opencv_tools import SimpleVideoReader
 import cv2
 import os 
-
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_dir = 'data/validation_videos/T1/long_segments'
 
-# mot_results2  = np.loadtxt('gt2.txt', delimiter=',')
+def generate_mot_from_cvat(cvat_results_filename, split_list, video_filename, clean_ids_list, skip_frames=0):
+    mot_results  = np.loadtxt(cvat_results_filename, delimiter=',')
+    if len(clean_ids_list):
+        mot_results = clean_cvat_annotation(mot_results,clean_ids_list)
+    # segments_ends = [853,1303,1984,2818,3509,4008,4685,5355,np.inf]
+    video = SimpleVideoReader(video_filename, skip_frames=0)
+    largest_index = 0
+    frame_interval = skip_frames+1
+    for segment_nb, segment_end in enumerate(split_list):
+        sequence_len = 0
+        sequence_name = '{}_segment_{}'.format(video_filename.split('/')[-1].split('.')[0],segment_nb)
+        sequence_dir = os.path.join(sequences_dir,sequence_name)
+        os.mkdir(sequence_dir)
+        seqmap_file = open(os.path.join(sequence_dir,'seqinfo.ini'),'w')
 
-# num_frames_1 = 6474
+        sequence_gt_dir = os.path.join(sequence_dir,'gt')
+        os.mkdir(sequence_gt_dir)
 
-# mot_results2[:,0]+=num_frames_1
-# mot_results2[:,1]+=mot_results1[:,1].max()
-
-# mot_results = np.concatenate([mot_results1,mot_results2])
-
-seqmaps_dir = os.path.join(output_dir,'seqmaps')
-os.mkdir(seqmaps_dir)
-seqmaps = open(os.path.join(seqmaps_dir,'surfrider-test.txt'),'w')
-seqmaps.write('name\n')
-sequences_dir = os.path.join(output_dir,'surfrider-test')
-os.mkdir(sequences_dir)
-
-mot_results1  = np.loadtxt('data/validation_videos/T1/CVAT/gt1.txt', delimiter=',')
-# segments_ends = [853,1303,1984,2818,3509,4008,4685,5355,np.inf]
-segments_ends = [np.inf]
-video = SimpleVideoReader('data/validation_videos/T1/CVAT/part1.mp4', skip_frames=0)
-largest_index = 0
-for segment_nb, segment_end in enumerate(segments_ends):
-    sequence_len = 0
-    sequence_name = 'part_1_segment_{}'.format(segment_nb)
-    sequence_dir = os.path.join(sequences_dir,sequence_name)
-    os.mkdir(sequence_dir)
-    seqmap_file = open(os.path.join(sequence_dir,'seqinfo.ini'),'w')
-
-    sequence_gt_dir = os.path.join(sequence_dir,'gt')
-    os.mkdir(sequence_gt_dir)
-
-    first_frame_added = None
-    writer = cv2.VideoWriter(filename=os.path.join(output_dir,sequence_name+'.mp4'), apiPreference=cv2.CAP_FFMPEG, fourcc=fourcc, fps=video.fps/2, frameSize=video.shape, params=None)
-    while True: 
-        ret, frame, frame_read_nb = video.read()
-        if ret and (frame_read_nb + 1 <= segment_end):
-            if frame_read_nb % 2 == 0: 
-                if first_frame_added is None: first_frame_added = frame_read_nb
-                writer.write(frame)
-                sequence_len+=1
-        else: 
-            break
-        
-    writer.release()
-    with open(os.path.join(sequence_gt_dir,'gt.txt'),'w') as out_file:
-        mot_results_for_segment = [mot_result for mot_result in mot_results1 if (mot_result[0] <= segment_end and mot_result[0] >= first_frame_added+1)]
-        for mot_result in mot_results_for_segment:
-            frame_id = int(mot_result[0])
-            track_id = int(mot_result[1])
-            left, top, width, height = mot_result[2:6]
-            center_x = left+width/2
-            center_y = top+height/2
-            if (frame_id - 1) % 2 == 0: 
-                frame_id_to_write = int((frame_id - 1) // 2 - first_frame_added // 2 + 1)
-                track_id_to_write = int(track_id - largest_index)
-                out_file.write('{},{},{},{},{},{},{},{}\n'.format(frame_id_to_write,
-                                                                track_id_to_write,
-                                                                center_x,
-                                                                center_y,
-                                                                -1,
-                                                                -1,
-                                                                1,
-                                                                -1))
-        largest_index = max(mot_result[1] for mot_result in mot_results_for_segment)
+        first_frame_added = None
+        writer = cv2.VideoWriter(filename=os.path.join(output_dir,sequence_name+'.mp4'), apiPreference=cv2.CAP_FFMPEG, fourcc=fourcc, fps=video.fps/frame_interval, frameSize=video.shape, params=None)
+        while True: 
+            ret, frame, frame_read_nb = video.read()
+            if ret and (frame_read_nb + 1 <= segment_end):
+                if frame_read_nb % frame_interval == 0: 
+                    if first_frame_added is None: first_frame_added = frame_read_nb
+                    writer.write(frame)
+                    sequence_len+=1
+            else: 
+                break
+            
+        writer.release()
+        with open(os.path.join(sequence_gt_dir,'gt.txt'),'w') as out_file:
+            mot_results_for_segment = [mot_result for mot_result in mot_results if (mot_result[0] <= segment_end and mot_result[0] >= first_frame_added+1)]
+            for mot_result in mot_results_for_segment:
+                frame_id = int(mot_result[0])
+                track_id = int(mot_result[1])
+                left, top, width, height = mot_result[2:6]
+                center_x = left+width/2
+                center_y = top+height/2
+                if (frame_id - 1) % frame_interval == 0: 
+                    frame_id_to_write = int((frame_id - 1) // frame_interval - first_frame_added // frame_interval + 1)
+                    track_id_to_write = int(track_id - largest_index)
+                    out_file.write('{},{},{},{},{},{},{},{}\n'.format(frame_id_to_write,
+                                                                    track_id_to_write,
+                                                                    center_x,
+                                                                    center_y,
+                                                                    -1,
+                                                                    -1,
+                                                                    1,
+                                                                    -1))
+            largest_index = max(mot_result[1] for mot_result in mot_results_for_segment)
 
 
-    seqmap_file.write('[Sequence]\nname={}\nimDir=img1\nframeRate={}\nseqLength={}\nimWidth=1920\nimHeight=1080\nimExt=.png'.format(sequence_name,video.fps/2,sequence_len))
-    seqmap_file.close()
-    seqmaps.write(sequence_name)
+        seqmap_file.write('[Sequence]\nname={}\nimDir=img1\nframeRate={}\nseqLength={}\nimWidth=1920\nimHeight=1080\nimExt=.png'.format(sequence_name,video.fps/2,sequence_len))
+        seqmap_file.close()
+        seqmaps.write(sequence_name)
 
 
-mot_results2  = np.loadtxt('data/validation_videos/T1/CVAT/gt2.txt', delimiter=',')
-# segments_ends = [844,2021,2692,3544,3999,4744,5171,6127,6889,np.inf]
-segments_ends = [np.inf]
-video = SimpleVideoReader('data/validation_videos/T1/CVAT/part2.mp4', skip_frames=0)
-largest_index = 0
-for segment_nb, segment_end in enumerate(segments_ends):
-    sequence_len = 0
-    sequence_name = 'part_2_segment_{}'.format(segment_nb)
-    sequence_dir = os.path.join(sequences_dir,sequence_name)
-    os.mkdir(sequence_dir)
-    seqmap_file = open(os.path.join(sequence_dir,'seqinfo.ini'),'w')
-
-    sequence_gt_dir = os.path.join(sequence_dir,'gt')
-    os.mkdir(sequence_gt_dir)
-
-    first_frame_added = None
-    writer = cv2.VideoWriter(filename=os.path.join(output_dir,sequence_name+'.mp4'), apiPreference=cv2.CAP_FFMPEG, fourcc=fourcc, fps=video.fps/2, frameSize=video.shape, params=None)
-    while True: 
-        ret, frame, frame_read_nb = video.read()
-        if ret and (frame_read_nb + 1 <= segment_end):
-            if frame_read_nb % 2 == 0: 
-                if first_frame_added is None: first_frame_added = frame_read_nb
-                writer.write(frame)
-                sequence_len+=1
-        else: 
-            break
-        
-    writer.release()
-    with open(os.path.join(sequence_gt_dir,'gt.txt'),'w') as out_file:
-        mot_results_for_segment = [mot_result for mot_result in mot_results2 if (mot_result[0] <= segment_end and mot_result[0] >= first_frame_added+1)]
-        for mot_result in mot_results_for_segment:
-            frame_id = int(mot_result[0])
-            track_id = int(mot_result[1])
-            left, top, width, height = mot_result[2:6]
-            center_x = left+width/2
-            center_y = top+height/2
-            if (frame_id - 1) % 2 == 0: 
-                frame_id_to_write = int((frame_id - 1) // 2 - first_frame_added // 2 + 1)
-                track_id_to_write = int(track_id - largest_index)
-                out_file.write('{},{},{},{},{},{},{},{}\n'.format(frame_id_to_write,
-                                                                track_id_to_write,
-                                                                center_x,
-                                                                center_y,
-                                                                -1,
-                                                                -1,
-                                                                1,
-                                                                -1))
-        largest_index = max(mot_result[1] for mot_result in mot_results_for_segment)
+def clean_cvat_annotation(mot_results, list_of_ids_to_remove):
 
 
-    seqmap_file.write('[Sequence]\nname={}\nimDir=img1\nframeRate={}\nseqLength={}\nimWidth=1920\nimHeight=1080\nimExt=.png'.format(sequence_name,video.fps/2,sequence_len))
-    seqmap_file.close()
-    seqmaps.write(sequence_name+'\n')
+    indices_to_keep = [i for i in range(len(mot_results)) if mot_results[i,1] not in list_of_ids_to_remove]
 
-seqmaps.close()
+    mot_results = mot_results[indices_to_keep]
+    original_ids = sorted(list(set(mot_results[:,1])))
+    new_ids_map =  {v:k+1 for k,v in enumerate(original_ids)}
+    for result_nb in range(len(mot_results)):
+        mot_results[result_nb,1] = new_ids_map[mot_results[result_nb,1]]
+
+
+    return mot_results
+
+
+
+if __name__ == '__main__':
+
+    output_dir = 'data/validation_videos/T1/long_segments_24fps'
+    seqmaps_dir = os.path.join(output_dir,'seqmaps')
+    os.mkdir(seqmaps_dir)
+    seqmaps = open(os.path.join(seqmaps_dir,'surfrider-test.txt'),'w')
+    seqmaps.write('name\n')
+    sequences_dir = os.path.join(output_dir,'surfrider-test')
+    os.mkdir(sequences_dir)
+
+    clean_ids_lists = [[5,6,9,12,21,22,24,25,27,28,29,30,31,32,34,35,37,39,47,48,49,56,58]]#,[7,17,19,25,37,44]]
+    cvat_results_filenames = ['data/validation_videos/T1/CVAT/gt_part_1.txt'] #,'data/validation_videos/T1/CVAT/gt_part_2.txt']
+    video_filenames = ['data/validation_videos/T1/CVAT/part_1.mp4'] #,'data/validation_videos/T1/CVAT/part_2.mp4']
+    split_lists = [[np.inf]] #[[853,1303,1984,2818,3509,4008,4685,5355,np.inf],[844,2021,2692,3544,3999,4744,5171,6127,6889,np.inf]]
+
+
+    for cvat_results_filename, video_filename, split_list, clean_ids_list in zip(cvat_results_filenames, video_filenames, split_lists, clean_ids_lists):
+        generate_mot_from_cvat(cvat_results_filename=cvat_results_filename, 
+                               split_list=split_list, 
+                               video_filename=video_filename, 
+                               clean_ids_list=clean_ids_list,
+                               skip_frames=1)
+
+    seqmaps.close()
+
 
