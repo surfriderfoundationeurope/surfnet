187dce83bf51147de4995bef65cf4ee8f3e78396
mchagneux <mathis.chagneux@telecom-paris.fr>
2021-07-01 14:51:12 +0200

    code to generate all sequences at once

--- src/build_mot_for_real_videos.py
+++ src/build_mot_for_real_videos.py
@@ -5,9 +5,8 @@ import cv2
 import os 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 from math import ceil
-def generate_mot_from_cvat(cvat_results_filename, video_filename, clean_ids_list, skip_frames=1, sequences_duration=30, blank_space_duration=3, no_segment=False):
-    mot_results  = np.loadtxt(cvat_results_filename, delimiter=',')
-    if len(clean_ids_list): mot_results = remap_ids(mot_results, clean_ids_list)
+
+def generate_mot_from_cvat(mot_results, video_filename, skip_frames=1, sequences_duration=30, blank_space_duration=3, no_segment=False, base_name='part'):
 
     video = SimpleVideoReader(video_filename)
     blank_space_length = int(blank_space_duration*video.fps)
@@ -30,8 +29,7 @@ def generate_mot_from_cvat(cvat_results_filename, video_filename, clean_ids_list
         
 
     for segment_nb, (segment_start, segment_end) in enumerate(segments):
-        video_name = video_filename.split('/')[-1].split('.')[0]
-        sequence_name = video_name if no_segment else '{}_segment_{}'.format(video_name, segment_nb) 
+        sequence_name = base_name if no_segment else '{}_segment_{}'.format(base_name, segment_nb) 
         sequence_dir = os.path.join(sequences_dir,sequence_name)
         os.mkdir(sequence_dir)
         seqmap_file = open(os.path.join(sequence_dir,'seqinfo.ini'),'w')
@@ -79,6 +77,8 @@ def generate_mot_from_cvat(cvat_results_filename, video_filename, clean_ids_list
 
 
 def remap_ids(mot_results, list_ids_to_remove=[]):
+    if not len(mot_results):
+        return mot_results
 
     if len(list_ids_to_remove):
         indices_to_keep = [i for i in range(len(mot_results)) if mot_results[i,1] not in list_ids_to_remove]
@@ -93,6 +93,28 @@ def remap_ids(mot_results, list_ids_to_remove=[]):
 
 
 
+def preprocess_cvat_results(video_filenames, cvat_results_filenames, clean_ids_lists, write=False):
+
+    num_frames = [int(SimpleVideoReader(video_filename).num_frames) for video_filename in video_filenames]
+    original_mot_results = [remap_ids(np.loadtxt(cvat_results_filename, delimiter=','), clean_ids_list) for cvat_results_filename, clean_ids_list in zip(cvat_results_filenames, clean_ids_lists)]
+
+    fused_mot_results = []
+    mot_results = original_mot_results[0]
+    fused_mot_results.extend([mot_result for mot_result in mot_results])
+    num_frames_processed = num_frames[0]
+    largest_index_processed = max(mot_result[1] for mot_result in fused_mot_results)
+
+    for mot_results, num_frames in zip(original_mot_results[1:], num_frames[1:]):
+        mot_results[:,0] += num_frames_processed
+        mot_results[:,1] += largest_index_processed
+        fused_mot_results.extend([mot_result for mot_result in mot_results])
+
+        num_frames_processed += num_frames
+        largest_index_processed = max(mot_result[1] for mot_result in fused_mot_results)
+
+
+    return np.array(fused_mot_results)
+
 
 
     
@@ -102,7 +124,8 @@ def remap_ids(mot_results, list_ids_to_remove=[]):
 
 if __name__ == '__main__':
 
-    output_dir = 'data/validation_videos/T2/long_segments'
+    output_dir = 'data/validation_videos/T3/short_segments'
+    # os.rmdir()
     seqmaps_dir = os.path.join(output_dir,'seqmaps')
     os.mkdir(seqmaps_dir)
     seqmaps = open(os.path.join(seqmaps_dir,'surfrider-test.txt'),'w')
@@ -110,21 +133,36 @@ if __name__ == '__main__':
     sequences_dir = os.path.join(output_dir,'surfrider-test')
     os.mkdir(sequences_dir)
 
-    # clean_ids_lists = [[5,6,7,8,9,12,21,22,24,25,27,28,30,31,32,34,35,37,39,47,48,49,56,58],[7,17,19,25,37,44]]
-    clean_ids_lists = [[]]
-    cvat_results_filenames = ['data/validation_videos/T2/CVAT/gt.txt']#,'data/validation_videos/T1/CVAT/gt_part_2.txt']
-    video_filenames = ['data/validation_videos/T2/full/T2_1080_px_converted.mp4']#,'data/validation_videos/T1/CVAT/part_2.mp4']
-    # split_lists = [[np.inf]] #[[853,1303,1984,2818,3509,4008,4685,5355,np.inf],[844,2021,2692,3544,3999,4744,5171,6127,6889,np.inf]]
+    video_filenames = ['data/validation_videos/T1/CVAT/part_1.mp4',
+                       'data/validation_videos/T1/CVAT/part_2.mp4']
+    cvat_results_filenames = ['data/validation_videos/T1/CVAT/gt_part_1.txt',
+                              'data/validation_videos/T1/CVAT/gt_part_2.txt'] 
+    clean_ids_lists = [[5,6,7,8,9,12,21,22,24,25,27,28,30,31,32,34,35,37,39,47,48,49,56,58],[7,17,19,25,37,44]]
+
+    mot_results_1 = preprocess_cvat_results(video_filenames = video_filenames, cvat_results_filenames=cvat_results_filenames, clean_ids_lists=clean_ids_lists)
+    video_filename_1 = 'data/validation_videos/T1/full/T1_1080_px_converted.mp4'
+    
+    video_filename_2 ='data/validation_videos/T2/full/T2_1080_px_converted.mp4'
+    mot_results_2 = preprocess_cvat_results(video_filenames = [video_filename_2], cvat_results_filenames='data/validation_videos/T2/CVAT/gt.txt', clean_ids_lists=[[]])
+
+    cvat_results_filenames = ['data/validation_videos/T3/CVAT/gt_part_1.txt',
+                              'data/validation_videos/T3/CVAT/gt_part_2.txt'] #,'data/validation_videos/T1/CVAT/gt_part_2.txt']
+    video_filenames = ['data/validation_videos/T3/CVAT/part_1.mp4',
+                       'data/validation_videos/T3/CVAT/part_2.mp4']
+
+    mot_results_3 = preprocess_cvat_results(video_filenames, cvat_results_filenames, clean_ids_lists=[[],[]])
+    video_filename_3 = 'data/validation_videos/T3/full/T3_1080_px_converted.mp4'
 
 
-    for cvat_results_filename, video_filename, clean_ids_list in zip(cvat_results_filenames, video_filenames, clean_ids_lists):
+    mot_results = [mot_results_1,mot_results_2,mot_results_3]
+    video_filenames = [video_filename_1,video_filename_2,video_filename_3]
+    base_names = ['part_1','part_2','part_3']
 
-        generate_mot_from_cvat(cvat_results_filename=cvat_results_filename, 
-                               video_filename=video_filename, 
-                               clean_ids_list=clean_ids_list,
-                               skip_frames=1, 
-                               no_segment=True)
-
-    seqmaps.close()
+    for mot_result, video_filename, base_name in zip(mot_results, video_filenames, base_names):
 
 
+        generate_mot_from_cvat(mot_results=mot_result, 
+                            video_filename=video_filename, 
+                            skip_frames=1, 
+                            no_segment=True,
+                            base_name=base_name)
--- src/overlay_tracking_results_on_video.py
+++ src/overlay_tracking_results_on_video.py
@@ -53,7 +53,7 @@ def main(args):
 
         detections_for_frame = results[frame_nb]
         for detection in detections_for_frame:
-            # frame = cv2.circle(frame, (int(detection[1]), int(detection[2])), 5, (0, 0, 255), -1)
+            frame = cv2.circle(frame, (int(detection[1]), int(detection[2])), 5, (0, 0, 255), -1)
             cv2.putText(frame, '{}'.format(detection[0]), (int(detection[1]), int(detection[2])+20), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
 
         if gt_filename is not None: 
