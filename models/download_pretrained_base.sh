1ac9f027cd497b6be123698539bd12e8310071e9
Chayma Mesbahi <chayma.mesbahi@neoxia.com>
2022-04-29 18:07:38 +0200

    update file models/download_pretrained_base.sh

--- models/download_pretrained_base.sh
+++ models/download_pretrained_base.sh
@@ -1,2 +1,2 @@
-wget https://partage.imt.fr/index.php/s/sJi22N6gedN6T4q/download
-mv download mobilenet_v3_pretrained.pth
+wget --no-check-certificate https://partage.imt.fr/index.php/s/sJi22N6gedN6T4q/download
+mv download models/mobilenet_v3_pretrained.pth
