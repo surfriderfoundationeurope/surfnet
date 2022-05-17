008bee84dcf24bac16bcbfb474c7192ea4e06992
Rispal <rispal@MacBook-Pro-de-Rispal.local>
2022-05-17 13:48:57 +0200

    orientation update

--- Repo_bene/src/models/image_orientation.py
+++ Repo_bene/src/models/image_orientation.py
@@ -1,26 +1,28 @@
 def image_orientation (image):
+    """ Function which gives the images that have a specified orientation the same orientation.
 
-    """ Function which gives the images that have a specified orientation the same orientation. 
+     Args:
+            image (image object): Image that is in the path data_directory as well as in the instance json files. 
 
-    Args: 
-        image : 
-    
-    Returns: 
-        
+        Returns: Image, with the propeer orinetation. 
+            _type_: image
     """
-    try:
-            for orientation in ExifTags.TAGS.keys():
-                if ExifTags.TAGS[orientation]=='Orientation':
-                    break
-            exif = image._getexif()
-            if exif is not None:
-                if exif[orientation] == 3:
-                    image=image.rotate(180, expand=True)
-                elif exif[orientation] == 6:
-                    image=image.rotate(270, expand=True)
-                elif exif[orientation] == 8:
-                    image=image.rotate(90, expand=True)
 
-        except (AttributeError, KeyError, IndexError):
-            # cases: image don't have getexif
-            pass
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation]=='Orientation':
+                break
+        exif = image._getexif()
+        if exif is not None:
+            if exif[orientation] == 3:
+                image=image.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                image=image.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                image=image.rotate(90, expand=True)
+
+    except (AttributeError, KeyError, IndexError):
+        # cases: image don't have getexif
+        pass
+
+    return (image)
