d4570a806a5fb05dfd2d000eb190f29cca355551
Rispal <rispal@MacBook-Pro-de-Rispal.local>
2022-05-17 16:51:01 +0200

    update of function

--- Repo_bene/src/models/get_df_train_val.py
+++ Repo_bene/src/models/get_df_train_val.py
@@ -5,13 +5,16 @@ def get_df_train_val(annotation_file):
     """Transforms the labels and images to the same format in order to be used by the Yolov5 algorithm. 
   
     Args: 
-        annotation_file (json instances file): Annotation file which contains information on the images, the annotations (labels) and categories of the labels. 
+        annotation_file (json instances file): Annotation file which contains information on the images, 
+        the annotations (labels) and categories of the labels. 
 
     Returns:
-        my_df (data frame): Data frame with columns : old_path, date, view, quality, context, img_name, label_name, image and bounding box. 
+        my_df (data frame): Data frame with columns : old_path, date, view, quality, context, img_name,
+         label_name, image and bounding box. 
     """
     
-    coco = COCO(annotation_file) # transform the file using a coco function where the COCO function loads a coco annotation file and prepares data structures 
+    coco = COCO(annotation_file) # transform the file using a coco function where the COCO function 
+                                 # loads a coco annotation file and prepares data structures 
     # gives the annotations into a coco api form ; helps the user in extracting annotations conveniently
 
     old_filenames  = [] 
