57cc844484c9f114c6e62dac81709f6a1648a489
Rispal <rispal@MacBook-Pro-de-Rispal.local>
2022-05-19 12:02:51 +0200

    update shpaing boxes

--- src/models/shaping_bboxes.py
+++ src/models/shaping_bboxes.py
@@ -1,10 +1,15 @@
-from Repo_bene.src.models.coco2yolo import coco2yolo
+import numpy as np
+from src.models.coco2yolo import coco2yolo
 
-def shaping_bboxes(anns:list):
+def shaping_bboxes(anns:list, ratio:float, target_h:float, target_w:int):
+
     """Function in charge of shaping the bounding boxes, normalized via the coco2yolo function.
 
     Args:
         anns (list): List with ID of the label, ID of the image, bounding box coordinates and the category ID
+        ratio (float): Ratio of the target (1080) and the actual height of the image. (defined in path_existance)
+        target_h (float): The target height of the image. (defined in path_existance)
+        target_w (float): The target width of the image: (ratio*width of actual image). (defined in path_existance)
     
     Returns: yolo_annot a list with the coordinates of the bboxes and their associated label. 
         _type_: list
@@ -18,9 +23,8 @@ def shaping_bboxes(anns:list):
         bbox = np.array([bbox_x, bbox_y, bbox_w, bbox_h]) # creates array with the bboxes coordinates 
         yolo_bbox = coco2yolo(bbox, target_h, target_w) # calls coco2yolo function to normalize the coordinates 
         yolo_str  = str(cat) + " " + " ".join(yolo_bbox.astype(str)) 
-        # gives the category of thee label and the coordinates of the bboxes 
+        # gives the category of the label and the coordinates of the bboxes 
         yolo_annot.append(yolo_str)
 
     return(yolo_annot)
     # list with the coordinates of the bboxes and their associated label
-
