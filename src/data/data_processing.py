324edc648d9899c3ee3a25ac29b6d7c8b2e00b23
Rispal <rispal@MacBook-Pro-de-Rispal.local>
2022-05-19 13:35:47 +0200

    data processing

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -1,2 +1,55 @@
 import numpy as np
 
+def coco2yolo(bbox:list, image_height:int=1080, image_width:int=1080):
+    
+    """Function to normalize the representation of the bounding box, such that there are in the yolo format (normalized)
+
+    Args:
+        bbox (list): Coordinates of the bounding box : x, y, w and h coordiates.
+        image_height (int, optional): Height of the image. Defaults to 1080.
+        image_width (int, optional): Width of the image. Defaults to 1080.
+
+    Returns: Normalized bounding box coordinates : values between 0-1.
+        _type_: array 
+    """
+    
+    bbox = bbox.copy().astype(float) # otherwise all value will be 0 as voc_pascal dtype is np.int
+
+    bbox[[0, 2]] = bbox[[0, 2]]/ image_width
+    bbox[[1, 3]] = bbox[[1, 3]]/ image_height
+    
+    bbox[[0, 1]] = bbox[[0, 1]] + bbox[[2, 3]]/2
+    
+    return bbox
+
+
+
+
+
+def shaping_bboxes(anns:list, ratio:float, target_h:float, target_w:int):
+
+    """Function in charge of shaping the bounding boxes, normalized via the coco2yolo function.
+
+    Args:
+        anns (list): List with ID of the label, ID of the image, bounding box coordinates and the category ID
+        ratio (float): Ratio of the target (1080) and the actual height of the image. (defined in path_existance)
+        target_h (float): The target height of the image. (defined in path_existance)
+        target_w (float): The target width of the image: (ratio*width of actual image). (defined in path_existance)
+    
+    Returns: yolo_annot a list with the coordinates of the bboxes and their associated label. 
+        _type_: list
+    """
+    yolo_annot = []
+
+    for ann in anns:
+        cat = ann['category_id'] - 1 # gets the actual categories, according to the initial yaml 
+        [bbox_x, bbox_y, bbox_w, bbox_h] = (ratio*np.array(ann['bbox'])).astype(int) 
+        # gets the bboxes coordinates * the ratio = (1080 /height of the image)
+        bbox = np.array([bbox_x, bbox_y, bbox_w, bbox_h]) # creates array with the bboxes coordinates 
+        yolo_bbox = coco2yolo(bbox, target_h, target_w) # calls coco2yolo function to normalize the coordinates 
+        yolo_str  = str(cat) + " " + " ".join(yolo_bbox.astype(str)) 
+        # gives the category of the label and the coordinates of the bboxes 
+        yolo_annot.append(yolo_str)
+
+    return(yolo_annot)
+    # list with the coordinates of the bboxes and their associated label
