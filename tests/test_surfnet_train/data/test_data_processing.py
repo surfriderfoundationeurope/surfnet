1ed28c1a8492814b4248e655a55834e0a0007586
Rispal <rispal@MacBook-Pro-de-Rispal.local>
2022-06-07 11:09:04 -0400

    unit test

--- tests/test_surfnet_train/data/test_data_processing.py
+++ tests/test_surfnet_train/data/test_data_processing.py
@@ -3,7 +3,6 @@ from src.surfnet_train.data.data_processing import coco2yolo
 from src.surfnet_train.data.data_processing import image_orientation
 from src.surfnet_train.data.data_processing import shaping_bboxes
 import numpy as np
-import PIL
 from PIL import Image, ImageChops, ImageDraw, ImageFont, ExifTags
 from pycocotools.coco import COCO
 import os
@@ -37,8 +36,8 @@ def test_image_orientation():
     f.close() # create our own json file with 2 images : "file.json"
 
     my_image = Image.open(os.path.join('tests/test_surfnet_train/utils/images', 
-    COCO('tests/test_surfnet_train/utils/data/file.json').loadImgs(1)[0]['file_name']))
-    # create the image in the same way as our function 
+    COCO('tests/test_surfnet_train/utils/data/file.json').loadImgs(333)[0]['file_name']))
+    # create the image in the same way as our function : did not see any change 
 
     output = image_orientation(my_image) 
 
@@ -47,10 +46,47 @@ def test_image_orientation():
 
     assert (output).size == my_image.size
     assert np.count_nonzero(cv2.subtract(ogimage, testimage)) == 0
-    
+    # here we're comparing the new image with the OG one : so should not work if we have a chnage in the function 
+
     #assert type(output) == PIL.Image.Image
     #assert np.testing.
     #all((cv2.subtract(ogimage, testimage) == 0))
     # assert equality 2 images ; array of pixel coordonnée : comparer les 2 tableaux 
     # assert my_image._getexif()[orientation] == []
     # assert diff = ImageChops.difference(im2, im1)
+
+
+
+
+
+def test_shaping_bboxes():
+
+    output = shaping_bboxes()
+
+def shaping_bboxes(anns:list, ratio:float, target_h:float, target_w:int):
+
+    """Function in charge of shaping the bounding boxes, normalized via the coco2yolo function.
+
+    Args:
+        anns (list): List with ID of the label, ID of the image, bounding box coordinates and the category ID
+        ratio (float): Ratio of the target (1080) and the actual height of the image. (defined in path_existance)
+        target_h (float): The target height of the image. (defined in path_existance)
+        target_w (float): The target width of the image: (ratio*width of actual image). (defined in path_existance)
+    
+    Returns: yolo_annot a list with the coordinates of the bboxes and their associated label. 
+        _type_: list
+    """
+    yolo_annot = []
+
+    for ann in anns:
+        cat = ann['category_id'] - 1 # gets the actual categories, according to the initial yaml 
+        [bbox_x, bbox_y, bbox_w, bbox_h] = (ratio*np.array(ann['bbox'])).astype(int) 
+        # gets the bboxes coordinates * the ratio = (1080 /height of the image)
+        bbox = np.array([bbox_x, bbox_y, bbox_w, bbox_h]) # creates array with the bboxes coordinates 
+        yolo_bbox = coco2yolo(bbox, target_h, target_w) # calls coco2yolo function to normalize the coordinates 
+        yolo_str  = str(cat) + " " + " ".join(yolo_bbox.astype(str)) 
+        # gives the category of the label and the coordinates of the bboxes 
+        yolo_annot.append(yolo_str)
+
+    return(yolo_annot)
+    # list with the coordinates of the bboxes and their associated label
