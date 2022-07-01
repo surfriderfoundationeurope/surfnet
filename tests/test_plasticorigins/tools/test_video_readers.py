eff0d4987952dd445efee50bf524a2de58a80ae4
Packophys <maxime.zennaro@neoxia.com>
2022-07-01 12:17:47 +0200

    add unittest for video readers

---
+++ tests/test_plasticorigins/tools/test_video_readers.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from plasticorigins.tools.video_readers import SimpleVideoReader
+from plasticorigins.serving.inference import config_track
+
+
+def test_track_video_simple():
+    config_track.video_path = "tests/ressources/validation_videos/T1_trim.mp4"
+    
+    reader = SimpleVideoReader(
+        video_filename=config_track.video_path,
+        skip_frames=config_track.skip_frames,
+    )
+
+    assert reader.read()[0]
+    assert reader.read()[1].shape == (360, 640, 3)
+    #assert reader.read()[-1] == 1
