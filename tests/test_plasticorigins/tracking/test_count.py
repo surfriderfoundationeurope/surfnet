e92ef0a2d8d5e43bdd7060c212a9c071f010e407
nousayma <>
2023-06-12 14:32:57 +0200

    Add unit test for object count in video.

---
+++ tests/test_plasticorigins/tracking/test_count.py
@@ -0,0 +1,19 @@
+from plasticorigins.tracking.utils import read_tracking_results
+from plasticorigins.tracking.count import (
+    count_detected_objects,
+    video_count_truth,
+)
+
+video_file_path = "tests/ressources/validation_videos/T1_trim.mp4"
+tracking_results_file = "tests/ressources/T1_trim_filtered_results.txt"
+
+
+def test_evaluate_detected_count():
+    results = read_tracking_results(tracking_results_file)
+    n_det, _ = count_detected_objects(results, video_file_path, compare=False)
+    assert n_det == 1
+
+
+def test_video_count_truth():
+    n = video_count_truth(video_file_path)
+    assert n == 2
