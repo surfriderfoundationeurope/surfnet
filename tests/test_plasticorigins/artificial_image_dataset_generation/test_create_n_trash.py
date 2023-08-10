cf1ccc9e6eddeb94831ab747e0699cc15f97d8fd
HibaTouderti <hiba.touderti@neoxia.com>
2023-08-10 16:52:46 +0200

    adding the artificial image dataset generation functions and tests

---
+++ tests/test_plasticorigins/artificial_image_dataset_generation/test_create_n_trash.py
@@ -0,0 +1,75 @@
+import subprocess
+import os
+import shutil
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+functions_dir = os.path.join(
+    current_dir, "../../../src/plasticorigins/training/artificial_image_dataset_generation")
+script_path = os.path.join(functions_dir, "create_n_trash.py")
+dataset_path = os.path.join(current_dir, "../../ressources/data_gen")
+background_dataset_path = os.path.join(
+    current_dir, "../../ressources/background_images")
+result_dataset_path = os.path.join(
+    current_dir, "../../ressources/artificial_data_n_objects")
+result_images_path = os.path.join(
+    result_dataset_path, "images")
+result_labels_path = os.path.join(
+    result_dataset_path, "labels")
+
+
+def test_num_images():
+    # remove the save directory if it does exist
+    if shutil.os.path.exists(result_dataset_path):
+        shutil.rmtree(result_dataset_path)
+
+    cmd = f'python {script_path} --dataset_path {dataset_path} --background_dataset_path {background_dataset_path} --result_dataset_path {result_dataset_path}'
+
+    # Run the shell command and capture the output
+    output = subprocess.check_output(cmd, shell=True)
+    images = os.listdir(result_images_path)
+    assert len(images) == 3
+    for file in images:
+        assert os.path.isfile(os.path.join(result_images_path, file))
+        file_extension = os.path.splitext(file)[1].lower()
+        assert file_extension == '.jpg'
+
+
+def test_num_labels():
+    # remove the save directory if it does exist
+    if shutil.os.path.exists(result_dataset_path):
+        shutil.rmtree(result_dataset_path)
+
+    cmd = f'python {script_path} --dataset_path {dataset_path} --background_dataset_path {background_dataset_path} --result_dataset_path {result_dataset_path}'
+
+    # Run the shell command and capture the output
+    output = subprocess.check_output(cmd, shell=True)
+    files = sorted(os.listdir(result_labels_path))
+
+    assert len(files) == 3
+    for file in files:
+        assert os.path.isfile(os.path.join(result_labels_path, file))
+        file_extension = os.path.splitext(file)[1].lower()
+        assert file_extension == '.txt'
+
+
+def test_label_components():
+    # remove the save directory if it does exist
+    if shutil.os.path.exists(result_dataset_path):
+        shutil.rmtree(result_dataset_path)
+
+    cmd = f'python {script_path} --dataset_path {dataset_path} --background_dataset_path {background_dataset_path} --result_dataset_path {result_dataset_path}'
+
+    # Run the shell command and capture the output
+    output = subprocess.check_output(cmd, shell=True)
+    files = os.listdir(result_labels_path)
+    files = sorted(files)
+    print(files)
+    for i in range(len(files)):
+        file_path = os.path.join(result_labels_path, files[i])
+
+        with open(file_path, 'r') as f:
+            line_count = sum(1 for _ in f)
+            if i == 1:
+                assert line_count == 2
+            else:
+                assert line_count == 1
---
+++ tests/test_plasticorigins/artificial_image_dataset_generation/test_utils.py
@@ -0,0 +1,89 @@
+import json
+import os
+import cv2
+import numpy as np
+from src.artificial_dataset_generation.utils import center_polygon, scale_img, rotate_img, shift_img, get_bbox_from_polygon
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+functions_dir = os.path.join(
+    current_dir, "../src/artificial_dataset_generation")
+script_path = os.path.join(functions_dir, "utils.py")
+dataset_path = os.path.join(current_dir, "ressources/data")
+background_dataset_path = os.path.join(
+    current_dir, "ressources/background_images")
+result_dataset_path = os.path.join(
+    current_dir, "ressources/artificial_data")
+result_images_path = os.path.join(
+    result_dataset_path, "images")
+result_labels_path = os.path.join(
+    result_dataset_path, "labels")
+
+def get_img_ann():
+    # Check if new x,y == center
+    file_name = "000004.jpg"
+    img_path = os.path.join(dataset_path, file_name)
+    ann_path = os.path.join(dataset_path, "annotations.json")
+
+    image = cv2.imread(img_path)
+    H, W = image.shape[:2]
+    img_size = (W, H)
+
+    with open(ann_path, 'r') as f:
+        dataset = json.loads(f.read())
+
+    img_id = [i['id'] for i in dataset['images']
+              if i['file_name'].lower() == file_name]
+    # print(img_id)
+    img_id = img_id[0]
+    annotations = [i for i in dataset['annotations']
+                   if i['image_id'] == img_id]
+
+    return image, img_size, annotations
+
+
+def test_center_polygon():
+    image, img_size, annotations = get_img_ann()
+    W, H = img_size
+    for ann in annotations:
+        print(ann['id'])
+        seg = ann['segmentation']
+        polygon = np.array(np.array(seg), np.int32)
+        polygon = polygon.reshape((-1, 2))
+        image, polygon = center_polygon(image, polygon)
+        bounding_box = get_bbox_from_polygon(polygon, img_size)
+        x, y, width, height = bounding_box
+
+        assert (x == W//2)
+        assert (y == H//2)
+
+
+def test_scale():
+    image, img_size, annotations = get_img_ann()
+    W, H = img_size
+    for ann in annotations:
+        seg = ann['segmentation']
+        polygon = np.array(np.array(seg), np.int32)
+        polygon = polygon.reshape((-1, 2))
+        image, polygon = scale_img(image, polygon)
+        bounding_box = get_bbox_from_polygon(polygon, img_size)
+        x, y, width, height = bounding_box
+
+        assert (width < 0.15*W) or (height < 0.15*H)
+        assert (width > 0.05*W) or (height > 0.05*H)
+
+
+def test_shift():
+    image, img_size, annotations = get_img_ann()
+    W, H = img_size
+    for ann in annotations:
+        seg = ann['segmentation']
+        polygon = np.array(np.array(seg), np.int32)
+        polygon = polygon.reshape((-1, 2))
+        image, polygon = shift_img(image, polygon)
+        bounding_box = get_bbox_from_polygon(polygon, img_size)
+        x, y, width, height = bounding_box
+
+        assert (x >= width//2)
+        assert (x <= W - width//2)
+        assert (y >= height//2)
+        assert (y <= H - height//2)
