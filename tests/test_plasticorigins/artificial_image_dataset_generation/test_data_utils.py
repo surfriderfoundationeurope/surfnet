cb37d0729c52c1f2594211ef01f411828dd300f2
Packophys <maximezen@DESKTOP-40I5283.localdomain>
2023-08-24 14:43:51 +0200

    change data paths

--- tests/test_plasticorigins/artificial_image_dataset_generation/test_data_utils.py
+++ tests/test_plasticorigins/artificial_image_dataset_generation/test_data_utils.py
@@ -6,13 +6,13 @@ from src.plasticorigins.training.artificial_image_dataset_generation.utils impor
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 functions_dir = os.path.join(
-    current_dir, "../src/artificial_dataset_generation")
+    current_dir, "../../../src/artificial_dataset_generation")
 script_path = os.path.join(functions_dir, "utils.py")
-dataset_path = os.path.join(current_dir, "ressources/data_gen")
+dataset_path = os.path.join(current_dir, "../../ressources/data_gen")
 background_dataset_path = os.path.join(
-    current_dir, "ressources/background_images")
+    current_dir, "../../ressources/background_images")
 result_dataset_path = os.path.join(
-    current_dir, "ressources/artificial_data")
+    current_dir, "../../ressources/artificial_data")
 result_images_path = os.path.join(
     result_dataset_path, "images")
 result_labels_path = os.path.join(
